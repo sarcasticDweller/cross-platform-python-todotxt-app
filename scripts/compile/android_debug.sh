f7f0aa5b2eb487bf71f4ef9c47781735ea79db54
sarcasticDweller <capslocked.strife@gmail.com>
2026-08-09 13:43:31 -0400

    Refactor compilation scripts

--- scripts/compile/android_debug.sh
+++ scripts/compile/android_debug.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
-JAVA_HOME=/usr/lib/jvm/temurin-17-jdk-amd64 uv run buildozer android debug deploy run
+./scripts/copmile/helpers/get_java.sh
+uv run buildozer android debug deploy run
 adb logcat | grep python
