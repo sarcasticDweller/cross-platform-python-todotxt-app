e32054e0bc02cb7648c96fb4da4a00be98bbcd57
sarcasticDweller <capslocked.strife@gmail.com>
2026-08-14 08:33:44 -0400

    Clean up merge gore in scripts

--- scripts/compile/android.sh
+++ scripts/compile/android.sh
@@ -1,11 +1,3 @@
 #!/usr/bin/env bash
-<<<<<<< HEAD
-<<<<<<< HEAD
-./scripts/compile/helper_get_java.sh
-=======
-./scripts/copmile/helpers/get_java.sh
->>>>>>> f7f0aa5b2eb487bf71f4ef9c47781735ea79db54
-=======
 source ./scripts/compile/helpers/get_java.sh
->>>>>>> 1daa29a (Implement file loading API and Android folder picker)
 uv run buildozer android debug
